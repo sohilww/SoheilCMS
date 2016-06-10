e7f92055a8ee3b71cd5969c458a45502fe880cbe
Soheil Karami <sohil_ww@ymail.com>
2016-06-11 01:05:26 +0430

    Commit

---
+++ Posts.DomainModel/ArticlesDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using FrameWork.Domain.Model;
+
+namespace Articles.DomainModel
+{
+    public class ArticlesDbContext:BaseDbContext<ArticlesDbContext>
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+
+    public class Category
+    {
+    }
+
+    public class Post
+    {
+    }
+}
