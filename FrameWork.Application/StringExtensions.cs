61657d1bc692b0b9a7d800c0352a94dcffd07008
Soheil Karami <sohil_ww@ymail.com>
2016-06-12 02:30:29 +0430

    Set Commect for String Extentions Class

--- FrameWork.Application/StringExtensions.cs
+++ FrameWork.Application/StringExtensions.cs
@@ -9,8 +9,16 @@ namespace FrameWork.Application
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Checking Slug Has White Space And Symbole Correcter
+        /// </summary>
+        /// <param name="slug">URL</param>
+        /// <returns></returns>
         public static string CreateFreandlySlug(this string slug)
         {
+
+            //\u0600-\u06FF\uFB8A\u067E\u0686\u06AF 
+            //Chacking Persian Correcter
             slug = new Regex(@"[^0-9 a-z \u0600-\u06FF\uFB8A\u067E\u0686\u06AF -]").Replace(slug, string.Empty);
             slug = slug.Replace(' ', '-');
            return slug;
