ca8dfa22438c8ba5d1a25a765dd96d43fc42acbf
Soheil Karami <sohil_ww@ymail.com>
2016-06-13 21:30:08 +0430

    Create Plugin Solution

---
+++ PluginBase/IPluginBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace PluginBase
+{
+    public interface  IPluginBase
+    {
+        MenuItem GetMenuItem(RequestContext requestContext);
+
+        List<MenuItem> GetChildMenuItem(RequestContext requestContext);
+
+
+        void RegisterBundles(BundleCollection bundles);
+        void RegisterRoutes(RouteCollection routes);
+    }
+}
---
+++ PluginBase/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace PluginBase
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+    }
+}
