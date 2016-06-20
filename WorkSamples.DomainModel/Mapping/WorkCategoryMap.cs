afa9605b9f27fa0ba5e7e2856894219162b5274b
Soheil Karami <sohil_ww@ymail.com>
2016-06-21 00:03:35 +0430

    Work Samples And WorkCategory Domain

---
+++ WorkSamples.DomainModel/Mapping/WorkCategoryMap.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace WorkSamples.DomainModel.Mapping
+{
+    public class WorkCategoryMap : EntityTypeConfiguration<WorkCategory>
+    {
+        public WorkCategoryMap()
+        {
+            ToTable("WorkCategories");
+
+
+            HasKey(a => a.Id);
+
+
+            Property(a => a.Id)
+                .HasDatabaseGeneratedOption(System.ComponentModel
+                .DataAnnotations.Schema.DatabaseGeneratedOption.None);
+
+            Property(a => a.CategoryImage).IsUnicode()
+                .IsOptional()
+                .HasMaxLength(400);
+
+
+            Property(a => a.Description).IsUnicode()
+                .IsOptional()
+                .HasMaxLength(300);
+
+            Property(a => a.KeyWord).IsUnicode()
+                .IsOptional()
+                .HasMaxLength(400);
+
+
+            Property(a => a.Slug).IsRequired()
+                .IsUnicode()
+                .HasMaxLength(150);
+
+            Property(a => a.Title).IsRequired()
+                .IsUnicode()
+                .HasMaxLength(150);
+
+
+            
+
+            HasOptional(a => a.Parent)
+                .WithMany(a => a.Children)
+                .HasForeignKey(a => a.ParentId);
+        }
+    }
+}
