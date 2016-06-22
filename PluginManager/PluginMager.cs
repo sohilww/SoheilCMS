91103817dbd2209a74e06af98d2c19e65ae7b9ac
Soheil Karami <sohil_ww@ymail.com>
2016-06-23 00:55:16 +0430

    Create Plugin Base And Plugin Manage

---
+++ PluginManager/PluginMager.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Modules;
+using PluginBase;
+using WorkSamplesPlugin;
+
+namespace PluginManager
+{
+    public class PluginMager:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IPluginBase>().To<WorkSamplesPuginBase>();
+        }
+    }
+}
