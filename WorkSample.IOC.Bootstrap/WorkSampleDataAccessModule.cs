9207069bb21317314d21796bbfcd964425cef8b7
Soheil Karami <sohil_ww@ymail.com>
2016-06-23 21:49:38 +0430

    WorkSample IOC Bootstraper

---
+++ WorkSample.IOC.Bootstrap/WorkSampleDataAccessModule.cs
@@ -0,0 +1,23 @@
+﻿using Ninject.Modules;
+using WorkSample.Data.DataAccess;
+using WorkSamples.Data.DataRepository;
+using WorkSamples.DomainModel;
+
+namespace WorkSample.IOC.Bootstrap
+{
+    public class WorkSampleDataAccessModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IWorkSampleRepository>().To<WorkSampleRepository>();
+            Bind<IWorkCategoryRepository>().To<WorkCategoryRepository>();
+        }
+        private void LoadDbContext()
+        {
+            Bind<IWorkSampleUnitofWork>().To<WorkSamplesUnitofWork>()
+                .InSingletonScope()
+                .WithConstructorArgument("WorkSampleDbContext", new WorkSampleDbContext());
+
+        }
+    }
+}
---
+++ WorkSamplesPlugin/Areas/WorkSamples/Controllers/WorkSampleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkSamplesPlugin.Areas.WorkSamples.Controllers
+{
+    public class WorkSampleController : Controller
+    {
+     
+        public WorkSampleController()
+        {
+            
+        }
+        // GET: WorkSamples/WorkSample
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
