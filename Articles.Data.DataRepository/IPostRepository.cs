67f5c219b5a62f0c63e8389cf65f398f8a90d8da
Soheil Karami <sohil_ww@ymail.com>
2016-06-26 02:06:36 +0430

    ShowPostInHomePage

--- Articles.Data.DataRepository/IPostRepository.cs
+++ Articles.Data.DataRepository/IPostRepository.cs
@@ -1,4 +1,6 @@
-﻿using Articles.DomainModel;
+﻿using System.Collections.Generic;
+using Articles.Contracts;
+using Articles.DomainModel;
 using FrameWork.Domain.Model;
 
 namespace Articles.Data.DataRepository
@@ -6,5 +8,6 @@ namespace Articles.Data.DataRepository
     public interface IPostRepository:IRepository<int,Post>
     {
         bool SlugExsist(string slug);
+        List<PostShowHomePage> HomePagePost(int count);
     }
 }
