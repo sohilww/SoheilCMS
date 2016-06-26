ed540827082910df2e3c90c1f25331615e7ad23d
Soheil Karami <sohil_ww@ymail.com>
2016-06-26 22:15:52 +0430

    Add Author Bussiness

---
+++ Authors.Application.Bussiness/AuthorService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Authors.Application.BussinessService;
+using Authors.Data.DataRepository;
+using Authors.DomainModel;
+using FrameWork.Application;
+
+namespace Authors.Application.Bussiness
+{
+    public class AuthorService : IAuthorService
+    {
+        IAuthorRepository rep;
+
+        public AuthorService(IAuthorRepository _rep)
+        {
+            rep = _rep;
+        }
+        public void Dispose()
+        {
+            // throw new NotImplementedException();
+        }
+
+        public Author Get(int id)
+        {
+            var result = rep.Get(id);
+            return result;
+        }
+
+        public EntityAction Create(Author entity)
+        {
+            entity.Id = rep.GetNextId();
+            EntityAction result = rep.Create(entity);
+            return result;
+        }
+
+        public EntityAction Update(Author entity)
+        {
+            EntityAction result = rep.Update(entity);
+            return result;
+        }
+
+        public EntityAction Delete(int id)
+        {
+            EntityAction result = rep.Delete(id);
+            return result;
+        }
+
+      
+
+        public int Count()
+        {
+            return rep.Count();
+        }
+
+        public List<Author> Select()
+        {
+            var model = rep.Select();
+            return model;
+        }
+    }
+}
---
+++ Authors.Application.BussinessService/ISiteMenuService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Authors.DomainModel;
+using FrameWork.Application;
+using FrameWork.Core;
+
+namespace Authors.Application.BussinessService
+{
+    public interface IAuthorService:IService
+    {
+        Author Get(int id);
+        EntityAction Create(Author entity);
+
+        EntityAction Update(Author entity);
+
+        EntityAction Delete(int id);
+
+        
+
+        int Count();
+
+        List<Author> Select();
+    }
+}
