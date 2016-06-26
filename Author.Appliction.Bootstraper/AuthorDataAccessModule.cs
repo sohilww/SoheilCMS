ba3cfb7d82f8459c302d4ca9401a68d99edf7120
Soheil Karami <sohil_ww@ymail.com>
2016-06-26 22:37:12 +0430

    Add Author Bootstrapper

---
+++ Author.Appliction.Bootstraper/AuthorDataAccessModule.cs
@@ -0,0 +1,26 @@
+﻿using Authors.Data.DataAccess;
+using Authors.Data.DataRepository;
+using Authors.DomainModel;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+namespace Author.Appliction.Bootstraper
+{
+    public class AuthorDataAccessModule : NinjectModule
+    {
+
+        public override void Load()
+        {
+            Bind<IAuthorUnitofWork>().To<AuthorUnitofWork>();
+            Bind<IAuthorRepository>().To<AuthorRepository>();
+        }
+        private void LoadDbContext()
+        {
+            Bind<IAuthorUnitofWork>().To<AuthorUnitofWork>()
+                .InRequestScope()
+                .WithConstructorArgument("MenuDbContext", new AuthorDbContext());
+
+        }
+
+    }
+}
---
+++ Author.Appliction.Bootstraper/AuthorServiceModule.cs
@@ -0,0 +1,14 @@
+﻿using Authors.Application.Bussiness;
+using Authors.Application.BussinessService;
+using Ninject.Modules;
+
+namespace Author.Appliction.Bootstraper
+{
+    public class AuthorServiceModule:NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IAuthorService>().To<AuthorService>();
+        }
+    }
+}
